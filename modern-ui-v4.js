582e377748420da990b1c43b006847b336c11089
kernix01 <sweetyvip4@gmail.com>
2026-07-13 05:35:25 +0500

    Delete modern-ui-v4.js

--- modern-ui-v4.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (!Array.isArray(window.GH_FIELDS)) {
-    window.GH_FIELDS = [
-      'ghc-token', 'ghc-name', 'ghc-visibility', 'ghc-desc',
-      'gh-username', 'gh-repo', 'gh-token', 'gh-filepath', 'gh-branch'
-    ];
-  }
-})();
-
